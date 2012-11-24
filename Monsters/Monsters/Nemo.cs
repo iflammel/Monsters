b083f3d30bf45729b68d5fe4a09402bb43ba86f0
iflammel <work@flammel.com>
2012-11-24 19:09:05 +0300

    Edit class Nemo

--- Monsters/Monsters/Nemo.cs
+++ Monsters/Monsters/Nemo.cs
@@ -9,6 +9,61 @@ namespace Monsters
 {
     class Nemo
     {
-        
+        Rectangle rect;
+        Texture2D run;
+        Texture2D idle;
+
+        bool isRunning = true;
+
+        int frameWidth;
+        int frameHeight;
+
+        public int Frames
+        {
+            get
+            {
+                return run.Width / frameWidth;
+            }
+        }
+
+        int currentFrame;
+        int timeElapsed;
+        int timeForFrame = 100;
+
+        public Nemo(Rectangle rect, Texture2D idle, Texture2D run)
+        {
+            this.rect = rect;
+            this.idle = idle;
+            this.run = run;
+
+            frameWidth = frameHeight = run.Height;
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            if (isRunning)
+            {
+                timeElapsed += gameTime.ElapsedGameTime.Milliseconds;
+                if (timeElapsed > timeForFrame)
+                {
+                    currentFrame = (currentFrame + 1) % Frames;
+                    timeElapsed = 0;
+                }
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            spriteBatch.Begin();
+            if (isRunning)
+            {
+                Rectangle r = new Rectangle(currentFrame * frameWidth, 0, frameWidth, frameHeight);
+            }
+            else
+            {
+                spriteBatch.Draw(idle, rect, Color.White);
+            }
+            spriteBatch.End();
+        }
     }
 }
